9644273a0381c734097a98a32a144f2e341ca3a0
simon3397 <simon3397@gmail.com>
2024-03-03 21:24:14 +0900

    [BUG_FIX]

--- app/src/main/java/com/byurin/trip/data/TaskDao.kt
+++ app/src/main/java/com/byurin/trip/data/TaskDao.kt
@@ -2,7 +2,6 @@ package com.byurin.trip.data
 
 import androidx.room.Dao
 import androidx.room.Delete
-import androidx.room.Entity
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
