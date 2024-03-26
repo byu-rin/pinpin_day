6068ab428cebf0357e96896435098408ff270265
simon3397 <simon3397@gmail.com>
2024-03-26 19:48:38 +0900

    [NEW] - timepicker custom

---
+++ app/src/main/java/com/byurin/trip/ui/add/CustomTimePicker.kt
@@ -0,0 +1,9 @@
+package com.byurin.trip.ui.add
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TimePicker
+
+class CustomTimePicker(context: Context, attrs: AttributeSet) : TimePicker(context, attrs) {
+
+}
