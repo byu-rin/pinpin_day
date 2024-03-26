78f78837bd497d94b901859e48779ce316423d4c
simon3397 <simon3397@gmail.com>
2024-03-27 01:17:05 +0900

    [BUG] - refactoring..

--- app/src/main/java/com/byurin/trip/ui/add/AddTasksActivity.kt
+++ app/src/main/java/com/byurin/trip/ui/add/AddTasksActivity.kt
@@ -2,7 +2,9 @@ package com.byurin.trip.ui.add
 
 import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.app.TimePickerDialog
+import android.os.Build.VERSION_CODES.O
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -23,163 +25,69 @@ class AddTasksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_add_tasks)
+        binding.lifecycleOwner = this  // Important for LiveData binding
+        binding.viewModel = viewModel
 
-        // LiveData 를 통해 선택된 시작 시간을 Observer 에게 전달
-        viewModel.startTime.observe(this, Observer { startTime ->
-            // LiveData 값 변경될 때마다 해당 tv 업데이트
-            binding.taskStartTime.text = startTime
-        })
+        setupObservers()
+    }
 
-        // LiveData 를 통해 선택된 종료 시간을 Observer 에게 전달
-        viewModel.endTime.observe(this, Observer { endTime ->
-            // LiveData 값 변경될 때마다 해당 tv 업데이트
-            binding.taskEndTime.text = endTime
-        })
+    private fun setupObservers() {
+        viewModel.startDate.observe(this) { date ->
+            binding.taskStartDate.text = date
+        }
 
-        // 현재 날짜와 시간으로 데이터 초기화
-        setupCurrentDate()
+        viewModel.startTime.observe(this) { time ->
+            binding.taskStartTime.text = time
+        }
 
+        viewModel.endDate.observe(this) { date ->
+            binding.taskEndDate.text = date
+        }
 
-        // 시작 날짜 클릭 시 DatePickerDialog 보이기
+        viewModel.endTime.observe(this) { time ->
+            binding.taskEndTime.text = time
+        }
+
         binding.taskStartDate.setOnClickListener {
-            showDatePicker(binding.taskStartDate)
+//            showDatePicker { year, month, dayOfMonth ->
+//                viewModel.updateStartDate(year, month, dayOfMonth)
+//            }
         }
 
-        // 종료 날짜 클릭 시 DatePickerDialog 보이기
-        binding.taskEndDate.setOnClickListener {
-            showDatePicker(binding.taskEndDate)
-        }
-
-        // 시작 시간 클릭 시 TimePickerDialog 보이기
         binding.taskStartTime.setOnClickListener {
-            showStartCustomTimePicker()
-        }
-
-        // 종료 시간 클릭 시 TimePickerDialog 보이기
-        binding.taskEndTime.setOnClickListener {
-            showEndCustomTimePicker()
+            showTimePicker { hour, minute ->
+                viewModel.updateStartTime(hour, minute)
+            }
         }
     }
 
-    // 시작 날짜와 현재 날짜를 디바이스 현재 날짜로 셋팅
-    private fun setupCurrentDate() {
-        // ViewModel 에서 현재 날짜 가져오기
-        val currrentDate = viewModel.getCurrentDate()
-        binding.taskStartDate.text = currrentDate
-        binding.taskEndDate.text = currrentDate
-
-    }
-
-    // 시간 클릭 시 custom timepicker 를 보여주는 함수
-    fun showStartCustomTimePicker() {
-        // custom TimePicker를 포함하는 레이아웃을 inflate
-        val customTimePickerView = layoutInflater.inflate(R.layout.custom_time_picker, null)
-
-        // custom TimePicker의 버튼 및 TimePicker를 참조
-        val timePicker = customTimePickerView.findViewById<TimePicker>(R.id.time_picker)
-        val setButton = customTimePickerView.findViewById<Button>(R.id.time_btn_yes)
-
-        // AlertDialog를 생성하고 custom TimePicker를 설정
-        val dialogBuilder = AlertDialog.Builder(this)
-            .setView(customTimePickerView)
-        val dialog = dialogBuilder.create()
-
-        // "set" 버튼 클릭 시 처리
-        setButton.setOnClickListener {
-            // 선택된 시간을 가져와서 처리할 수 있습니다.
-            val hour = timePicker.hour
-            val minute = timePicker.minute
-            // 선택한 시간을 TextView에 표시
-            viewModel.setStartTime(hour, minute)
-//            viewModel.setEndTime(hour, minute)
-
-            // 다 처리한 후에는 다이얼로그를 닫습니다.
-            dialog.dismiss()
-        }
-
-//        // LiveData 변경 감지를 위해 Observer 설정
-//        timeLiveData.observe(this, Observer { time ->
-//            // LiveData 값이 변경될 때마다 TimePicker를 업데이트합니다.
-//            val timeSplit = time.split(":")
-//            val hour = timeSplit[0].toInt()
-//            val minute = timeSplit[1].toInt()
-//            timePicker.hour = hour
-//            timePicker.minute = minute
-//        })
-
-        // 다이얼로그를 표시합니다.
-        dialog.show()
-    }
-
-    fun showEndCustomTimePicker() {
-        // custom TimePicker를 포함하는 레이아웃을 inflate
-        val customTimePickerView = layoutInflater.inflate(R.layout.custom_time_picker, null)
-
-        // custom TimePicker의 버튼 및 TimePicker를 참조
-        val timePicker = customTimePickerView.findViewById<TimePicker>(R.id.time_picker)
-        val setButton = customTimePickerView.findViewById<Button>(R.id.time_btn_yes)
-
-        // AlertDialog를 생성하고 custom TimePicker를 설정
-        val dialogBuilder = AlertDialog.Builder(this)
-            .setView(customTimePickerView)
-        val dialog = dialogBuilder.create()
-
-        // "set" 버튼 클릭 시 처리
-        setButton.setOnClickListener {
-            // 선택된 시간을 가져와서 처리할 수 있습니다.
-            val hour = timePicker.hour
-            val minute = timePicker.minute
-            // 선택한 시간을 TextView에 표시
-//            viewModel.setStartTime(hour, minute)
-            viewModel.setEndTime(hour, minute)
-
-            // 다 처리한 후에는 다이얼로그를 닫습니다.
-            dialog.dismiss()
-        }
-
-//        // LiveData 변경 감지를 위해 Observer 설정
-//        timeLiveData.observe(this, Observer { time ->
-//            // LiveData 값이 변경될 때마다 TimePicker를 업데이트합니다.
-//            val timeSplit = time.split(":")
-//            val hour = timeSplit[0].toInt()
-//            val minute = timeSplit[1].toInt()
-//            timePicker.hour = hour
-//            timePicker.minute = minute
-//        })
-
-        // 다이얼로그를 표시합니다.
-        dialog.show()
-    }
-
-//     날짜 클릭 시 DatePickerDialog 를 보여주는 함수
-    private fun showDatePicker(textView: TextView) {
-        val datePickerDialog =
-            DatePickerDialog(this, null, viewModel.year, viewModel.month, viewModel.day)
-        // datePickerDialog 의 header 숨김
-        datePickerDialog.datePicker.findViewById<View>(
-            resources.getIdentifier(
-                "date_picker_header",
-                "id",
-                "android"
-            )
-        )?.visibility = View.GONE
-
-        // DatePickerDialog 에서 날짜 선택 시 호출되는 콜백 설정
-        datePickerDialog.datePicker.init(
-            viewModel.year,
-            viewModel.month,
-            viewModel.day
-        ) { view, year, monthOfYear, dayOfMonth ->
-            // 선택된 날짜를 tv 에 반영
-            textView.text = "${monthOfYear + 1}월 ${dayOfMonth}일"
-            // 날짜 선택 후 dialog 닫기
-            datePickerDialog.dismiss()
-        }
-        datePickerDialog.show()
-    }
-
-    override fun onResume() {
-        super.onResume()
-
+    private fun showTimePicker(dataSetListener: (hour: Int, minute: Int) -> Unit) {
+//        val timePickerDialog = TimePickerDialog(this, { _, hour, minute ->
     }
 }
+
+//private fun showDatePicker(textView: TextView) {
+//    val datePickerDialog =
+//        DatePickerDialog(this, null, viewModel.year, viewModel.month, viewModel.day)
+//    // datePickerDialog 의 header 숨김
+//    datePickerDialog.datePicker.findViewById<View>(
+//        resources.getIdentifier(
+//            "date_picker_header",
+//            "id",
+//            "android"
+//        )
+//    )?.visibility = View.GONE
+//
+//    // DatePickerDialog 에서 날짜 선택 시 호출되는 콜백 설정
+//    datePickerDialog.datePicker.init(
+//        viewModel.year,
+//        viewModel.month,
+//        viewModel.day
+//    ) { view, year, monthOfYear, dayOfMonth ->
+//        // 선택된 날짜를 tv 에 반영
+//        textView.text = "${monthOfYear + 1}월 ${dayOfMonth}일"
+//        // 날짜 선택 후 dialog 닫기
+//        datePickerDialog.dismiss()
+//    }
+//    datePickerDialog.show()
+//}
--- app/src/main/java/com/byurin/trip/ui/add/AddTasksViewModel.kt
+++ app/src/main/java/com/byurin/trip/ui/add/AddTasksViewModel.kt
@@ -1,5 +1,10 @@
 package com.byurin.trip.ui.add
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Context
+import android.os.Build.VERSION_CODES.M
+import android.widget.TextView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,39 +14,48 @@ import java.util.Locale
 
 class AddTasksViewModel : ViewModel() {
 
-    // 디바이스에서 날짜 시간 가져오기
-    val currentDate = Calendar.getInstance()
-    val year = currentDate.get(Calendar.YEAR)
-    val month = currentDate.get(Calendar.MONTH)
-    val day = currentDate.get(Calendar.DAY_OF_MONTH)
+    private val _startDate = MutableLiveData<String>()
+    val startDate: LiveData<String> = _startDate
 
-
-    // 선택된 시작 시간과 종료 시간을 LiveData로 관리
     private val _startTime = MutableLiveData<String>()
+    val startTime: LiveData<String> = _startTime
+
+    private val _endDate = MutableLiveData<String>()
+    val endDate: LiveData<String> = _endDate
+
     private val _endTime = MutableLiveData<String>()
+    val endTime: LiveData<String> = _endTime
 
-    val startTime: LiveData<String>
-        get() = _startTime
-
-    val endTime: LiveData<String>
-        get() = _endTime
-
-    // SimpleDateFormat 초기화하여 날짜 표시하기 (월,일)
     private val dateFormat = SimpleDateFormat("M월 d일", Locale.getDefault())
+    private val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
 
-    // 현재 날짜 반환 함수
-    fun getCurrentDate(): String {
-        return dateFormat.format(currentDate.time)
+    init {
+        val currentCalendar = Calendar.getInstance()
+        _startDate.value = dateFormat.format(currentCalendar.time)
+        _startTime.value = timeFormat.format(currentCalendar.time)
+        _endDate.value = dateFormat.format(currentCalendar.time)
+        _endTime.value = timeFormat.format(currentCalendar.time)
     }
 
-    // 시작 시간 업데이트
-    fun setStartTime(hour: Int, minute: Int) {
-        // 선택한 시간과 분을 형식에 맞춰서 LiveData 에 저장
-        _startTime.value = String.format("%02d:%02d", hour, minute)
+    fun updateStartDate(year: Int, month: Int, dayOfMonth: Int) {
+        /* 원본
+        //val calendar = Calendar.getInstance().apply {
+        //            set(Calendar.HOUR_OF_DAY, hourOfDay)
+        //            set(Calendar.MINUTE, minute)
+        */
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.YEAR, year)
+            set(Calendar.MONTH, month)
+            set(Calendar.DAY_OF_MONTH, dayOfMonth)
+        }
+        _startDate.value = dateFormat.format(calendar.time)
     }
 
-    // 종료 시간 업데이트
-    fun setEndTime(hour: Int, minute: Int) {
-        _endTime.value = String.format("%02d:%02d", hour, minute)
+    fun updateStartTime(hourOfDay: Int, minute: Int) {
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, hourOfDay)
+            set(Calendar.MINUTE, minute)
+        }
+        _startTime.value = timeFormat.format(calendar.time)
     }
 }
